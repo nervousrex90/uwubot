e599bfb9490a67b9054c62c0aebd5ea6934659e7
NervousRex <capnkt626@gmail.com>
2020-09-02 14:57:32 -0400

    added karl

--- bot.js
+++ bot.js
@@ -58,7 +58,10 @@ client.on('message', message => {
 	}
 	if (message.guild.id === '740929953411104770'){
 		if (message.content.toLowerCase().includes('rock and stone')) {
-			message.channel.send("Rock and Stone!");
+			message.channel.send("ROCK AND STONE!");
+		}
+		if (message.content.toLowerCase().includes('karl')) {
+			message.channel.send("FOR KARL!");
 		}
 	} 
 });
