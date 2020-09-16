66dd5dde492cb82fb4288a0b8ea5a5c3a9a0aed8
NervousRex <capnkt626@gmail.com>
2020-09-16 11:24:44 -0400

    changed some gifs

--- bot.js
+++ bot.js
@@ -2,8 +2,8 @@
 const Discord = require('discord.js');
 var auth = process.env.TOKEN;
 const gifs = [
-    'https://media.giphy.com/media/LkfjHA9dIVYgJFmPOF/giphy.gif',
-    'https://media.giphy.com/media/iCeG5FbXeuHiE/giphy.gif',
+    'https://media1.tenor.com/images/b9ee278c57ad53283535860de5f48b4a/tenor.gif?itemid=16391139',
+    'https://media1.tenor.com/images/1f85c80528917efd023699de5d3445a1/tenor.gif?itemid=16018927',
     'https://media.giphy.com/media/4NZxtNA8Ef5L2/giphy.gif',
     'https://media.giphy.com/media/iNwDIhUl9S6j42lJz7/giphy.gif',
     'https://media.giphy.com/media/pHH0RsynZHGlG/giphy.gif',
@@ -11,12 +11,13 @@ const gifs = [
 	'https://media.giphy.com/media/hhHcFH0xAduCs/giphy.gif',
 ];
 const poggifs = [
-	'https://media1.tenor.com/images/9c84ae4d331de77c86968e4ea1b81cf0/tenor.gif?itemid=8179196',
-	'https://tenor.com/view/pogchamp-pepe-dance-pepechamp-emote-gif-9358907',
-	'https://tenor.com/view/pogchamp-pog-pogey-poggers-twitch-gif-14340727',
-	'https://media1.tenor.com/images/30e1029fd63cb44bdb22e721d8454792/tenor.gif?itemid=17487624',
+	'https://media.giphy.com/media/1ykTax6hrAKpTQ0Mnb/giphy.gif',
+	'https://media.giphy.com/media/m48e80jhv4Kk/giphy.gif',
+	'https://media1.tenor.com/images/ac86a2659730f38b5d22a10ac63d8039/tenor.gif?itemid=3557105',
+	'https://media1.tenor.com/images/12e3eeafee0e3ceb87201a2542f72e38/tenor.gif?itemid=3835045',
 	'https://media1.tenor.com/images/58fde03dc4c22c6bb55ec0a3c5d6c749/tenor.gif?itemid=10111831',
-	'https://media1.tenor.com/images/ea84f2f5ae572cfa8ec7e37b8d9e05f4/tenor.gif?itemid=17223821',
+	'https://media1.tenor.com/images/ded88848be37f78d5183dec89b714d96/tenor.gif?itemid=9358907',
+	'https://media1.tenor.com/images/30e1029fd63cb44bdb22e721d8454792/tenor.gif?itemid=17487624',
 ];
 const userID = '374655744345374730'
 
