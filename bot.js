1ec2a6b457ae608c5a063089528b0906922308a2
NervousRex <capnkt626@gmail.com>
2021-05-30 21:20:45 -0400

    changed gifs to reactions

--- bot.js
+++ bot.js
@@ -1,30 +1,19 @@
 // require the discord.js module
 const Discord = require('discord.js');
 var auth = process.env.TOKEN;
-const gifs = [
-    'https://media1.tenor.com/images/b9ee278c57ad53283535860de5f48b4a/tenor.gif?itemid=16391139',
-    'https://media1.tenor.com/images/1f85c80528917efd023699de5d3445a1/tenor.gif?itemid=16018927',
-    'https://media.giphy.com/media/4NZxtNA8Ef5L2/giphy.gif',
-    'https://media.giphy.com/media/iNwDIhUl9S6j42lJz7/giphy.gif',
-    'https://media.giphy.com/media/pHH0RsynZHGlG/giphy.gif',
-	'https://media.giphy.com/media/wkW0maGDN1eSc/giphy.gif',
-	'https://media.giphy.com/media/hhHcFH0xAduCs/giphy.gif',
+const uwureaction = [
+    'message.guild.emojis.cache.find(emoji => emoji.name === 'muwu'',
+	'message.guild.emojis.cache.find(emoji => emoji.name === 'CitrusUWU'',
 ];
-const poggifs = [
-	'https://media.tenor.com/images/79b62ed803d210a441d589bb874ddeae/tenor.gif',
-	'https://media.tenor.com/images/123a68b46c5f291619358a2efb94c70a/tenor.gif',
-	'https://media.tenor.com/images/74e5f8b76795875f9b06a05b4c6ee842/tenor.gif',
-	'https://media.tenor.com/images/b1600916c13300a8e414bfaa474d84f8/tenor.gif',
-	'https://media1.tenor.com/images/58fde03dc4c22c6bb55ec0a3c5d6c749/tenor.gif?itemid=10111831',
-	'https://media1.tenor.com/images/9428971d6d175764dc13c719740114e1/tenor.gif?itemid=19168029',
-	'https://media1.tenor.com/images/30e1029fd63cb44bdb22e721d8454792/tenor.gif?itemid=17487624',
+const pogreaction = [
+	'message.guild.emojis.cache.find(emoji => emoji.name === 'rivenpog'',
+	'message.guild.emojis.cache.find(emoji => emoji.name === 'DipsPog'',
+	'message.guild.emojis.cache.find(emoji => emoji.name === 'rivenpog'',
+	'message.guild.emojis.cache.find(emoji => emoji.name === 'hyperpogcat'',
+	'message.guild.emojis.cache.find(emoji => emoji.name === 'poggies'',
 ];
-const pepegifs = [
-	'https://media1.tenor.com/images/f550faf07f1493f22e1daf93568eadd3/tenor.gif?itemid=12683546',
-	'https://media1.tenor.com/images/bf9c687cbaf1b5b2ef3caa9075a6bae8/tenor.gif?itemid=17946795',
-	'https://media1.tenor.com/images/82168e2a49530193a66b0396f925aba0/tenor.gif?itemid=18805786',
-	'https://media1.tenor.com/images/d1dbb3a232bd162527ff0ae6732e5d7e/tenor.gif?itemid=16009739',
-	'https://media1.tenor.com/images/66b2e11e4c3f5488b3700dcc273728a7/tenor.gif?itemid=18126506',
+const pepereaction = [
+	'message.guild.emojis.cache.find(emoji => emoji.name === 'PepeLazerREEEE'',
 ];
 const catjam = [
 	'https://media1.tenor.com/images/ad8183f947907fff72624b99098b368f/tenor.gif?itemid=17946989'
@@ -58,16 +47,16 @@ client.login(auth);
 client.on('message', message => {
 	if (message.author.bot) return;
 	if (message.content.toLowerCase().includes('uwu')) {
-		message.channel.send(random_item(gifs));
+		message.react(uwureaction));
 	}
 	if (message.content.toLowerCase().includes('pog')) {
-		message.channel.send(random_item(poggifs));
+		message.react(pogreaction));
 	}
 	if (message.content.toLowerCase().includes('catjam')) {
 		message.channel.send(random_item(catjam));
 	}
 	if (message.content.toLowerCase().includes('pepe')) {
-		message.channel.send(random_item(pepegifs));
+		message.react(pepereaction));
 	}
 	if (message.content.includes('1v1 me')) {
 		let user = message.mentions.users.first();
